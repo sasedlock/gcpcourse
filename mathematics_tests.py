ff48911e67b60f8dcff712d1b61fd8a2e60cc8f0
Sean Sedlock <sean.sedlock@gmail.com>
2025-05-08 21:08:14 -0500

    Red!

--- mathematics_tests.py
+++ mathematics_tests.py
@@ -10,5 +10,9 @@ class TestMathematicsMethods(unittest.TestCase):
     def test_add_strings(self):
         self.assertEqual(mathematics.add("1","2"), 3)
 
+    # Passing an array of numbers as integers should return sum
+    def test_add_array_of_integers(self):
+        self.assertEqual(mathematics.add_array([1,2],3))
+
 if __name__ == '__main__':
     unittest.main()
